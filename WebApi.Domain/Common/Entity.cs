3d4a85887ad6c04e5e4b8530e4d754ec03bf8b21
kuairen <kaito_127@icloud.com>
2025-09-05 12:16:53 +0000

    feat(domain): :monocle_face: 監査情報を基底クラスに追加
    インフラストラクチャ層で管理予定だったが、DTOへの変換を簡易的にするためドメイン層に移動

--- WebApi.Domain/Common/Entity.cs
+++ WebApi.Domain/Common/Entity.cs
@@ -1,6 +1,28 @@
+using WebApi.Domain.Abstractions;
+
 namespace WebApi.Domain.Common;
 
 public abstract class Entity
 {
     public Guid Id { get; protected set; } = Guid.NewGuid();
+    public Guid CreatedBy { get; }
+    public DateTimeOffset CreatedAt { get; }
+    public Guid UpdatedBy { get; private set; }
+    public DateTimeOffset UpdatedAt { get; private set; }
+
+    protected Entity() { } // EF Core 用
+
+    protected Entity(Guid createdBy, IDateTimeProvider clock)
+    {
+        CreatedBy = createdBy;
+        CreatedAt = clock.Now;
+        UpdatedBy = createdBy;
+        UpdatedAt = clock.Now;
+    }
+
+    public void UpdateAuditInfo(Guid updatedBy, IDateTimeProvider clock)
+    {
+        UpdatedBy = updatedBy;
+        UpdatedAt = clock.Now;
+    }
 }
