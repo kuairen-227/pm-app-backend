9c02783f0993bdcfec17918b6c35cd877422e11a
kuairen <kaito_127@icloud.com>
2025-09-05 12:20:01 +0000

    refactor(domain): :truck: リポジトリインターフェースを `Abstractions` フォルダに移動

--- WebApi.Domain/Abstractions/Repositories/IProjectRepository.cs
+++ WebApi.Domain/Abstractions/Repositories/IProjectRepository.cs
@@ -1,10 +1,12 @@
-namespace WebApi.Domain.Aggregates.ProjectAggregate;
+using WebApi.Domain.Aggregates.ProjectAggregate;
+
+namespace WebApi.Domain.Abstractions.Repositories;
 
 public interface IProjectRepository
 {
     Task<IEnumerable<Project>> GetAllAsync(CancellationToken cancellationToken = default);
     Task<Project?> GetByIdAsync(Guid projectId, CancellationToken cancellationToken = default);
-    Task AddAsync(Project project, CancellationToken cancellationToken = default);
+    Task<Project> AddAsync(Project project, CancellationToken cancellationToken = default);
     Task UpdateAsync(Project project, CancellationToken cancellationToken = default);
     Task DeleteAsync(Project project, CancellationToken cancellationToken = default);
 }
--- WebApi.Domain/Abstractions/Repositories/ITicketRepository.cs
+++ WebApi.Domain/Abstractions/Repositories/ITicketRepository.cs
@@ -6,7 +6,7 @@ public interface ITicketRepository
 {
     Task<IEnumerable<Ticket>> GetAllAsync(Guid projectId, CancellationToken cancellationToken = default);
     Task<Ticket?> GetByIdAsync(Guid ticketId, CancellationToken cancellationToken = default);
-    Task AddAsync(Ticket ticket, CancellationToken cancellationToken = default);
+    Task<Ticket> AddAsync(Ticket ticket, CancellationToken cancellationToken = default);
     Task UpdateAsync(Ticket ticket, CancellationToken cancellationToken = default);
     Task DeleteAsync(Ticket ticket, CancellationToken cancellationToken = default);
 }
