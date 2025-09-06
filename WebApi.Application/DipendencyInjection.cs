8a105bda7a3c4e16c47fb57df51e167a7a040e2d
kuairen <kaito_127@icloud.com>
2025-09-06 04:21:15 +0000

    fix(application): :fire: 現状 `FluentValidation` を使用していないので削除

--- WebApi.Application/DipendencyInjection.cs
+++ WebApi.Application/DipendencyInjection.cs
@@ -1,6 +1,3 @@
-using System.Reflection;
-using FluentValidation;
-using FluentValidation.AspNetCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace WebApi.Application;
@@ -11,11 +8,6 @@ public static class DependencyInjection
     {
         // MediatR のハンドラをアセンブリから自動登録
         services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
-
-        // services.AddValidatorsFromAssemblyContaining<>;
-
-        // Fluent Validation の自動検証パイプラインを登録
-        services.AddFluentValidationAutoValidation();
 
         return services;
     }
