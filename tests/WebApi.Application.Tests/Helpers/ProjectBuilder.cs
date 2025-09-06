107b872d37824d852dcabbdcc989cac56a4b8351
kuairen <kaito_127@icloud.com>
2025-09-06 04:16:27 +0000

    feat(tests): :technologist: アプリケーション層テスト用の `ProjectBuilder` を作成

---
+++ tests/WebApi.Application.Tests/Helpers/ProjectBuilder.cs
@@ -0,0 +1,40 @@
+using WebApi.Application.Helpers.Common;
+using WebApi.Domain.Aggregates.ProjectAggregate;
+
+namespace WebApi.Domain.Tests.Helpers;
+
+public class ProjectBuilder : BaseBuilder<ProjectBuilder, Project>
+{
+    private string _name = "デフォルトプロジェクト";
+    private string? _description;
+    private Guid _ownerId = Guid.NewGuid();
+
+    public ProjectBuilder WithName(string name)
+    {
+        _name = name;
+        return this;
+    }
+
+    public ProjectBuilder WithDescription(string? description)
+    {
+        _description = description;
+        return this;
+    }
+
+    public ProjectBuilder WithOwnerId(Guid ownerId)
+    {
+        _ownerId = ownerId;
+        return this;
+    }
+
+    public override Project Build()
+    {
+        return new Project(
+            _name,
+            _description,
+            _ownerId,
+            _createdBy,
+            _clock
+        );
+    }
+}
