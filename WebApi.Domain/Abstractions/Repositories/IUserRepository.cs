ac7822eeab29f1cbf42c60156cd94944e8d8e3cd
kuairen <kaito_127@icloud.com>
2025-09-07 03:57:31 +0000

    fix(domain): :bug: 不要な戻り値を削除
    ドメイン層で生成したIDをもとに `GetByIdAsync` すれば十分なため

--- WebApi.Domain/Abstractions/Repositories/IUserRepository.cs
+++ WebApi.Domain/Abstractions/Repositories/IUserRepository.cs
@@ -4,7 +4,7 @@ public interface IUserRepository
 {
     Task<IEnumerable<User>> GetAllAsync(CancellationToken cancellationToken = default);
     Task<User?> GetByIdAsync(Guid userId, CancellationToken cancellationToken = default);
-    Task<User> AddAsync(User user, CancellationToken cancellationToken = default);
+    Task AddAsync(User user, CancellationToken cancellationToken = default);
     Task UpdateAsync(User user, CancellationToken cancellationToken = default);
     Task DeleteAsync(User user, CancellationToken cancellationToken = default);
 }
