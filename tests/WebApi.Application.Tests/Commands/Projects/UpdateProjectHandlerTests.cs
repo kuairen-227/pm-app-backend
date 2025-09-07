d5ab5981d346b1efb01ee40ed73af012e2e9795f
kuairen <kaito_127@icloud.com>
2025-09-07 04:59:20 +0000

    test(application): :white_check_mark: `UpdateProjectHandler` のテスト追加

---
+++ tests/WebApi.Application.Tests/Commands/Projects/UpdateProjectHandlerTests.cs
@@ -0,0 +1,86 @@
+using FluentAssertions;
+using MediatR;
+using Moq;
+using WebApi.Application.Commands.Projects.UpdateProject;
+using WebApi.Application.Common;
+using WebApi.Domain.Abstractions.Repositories;
+using WebApi.Domain.Aggregates.ProjectAggregate;
+using WebApi.Domain.Tests.Helpers;
+using WebApi.Domain.Tests.Helpers.Common;
+
+namespace WebApi.Application.Tests.Commands.Projects;
+
+public class UpdateProjectHandlerTests : BaseTest
+{
+    private readonly UpdateProjectHandler _handler;
+    private readonly Mock<IProjectRepository> _projectRepository;
+    private readonly ProjectBuilder _projectBuilder;
+
+    public UpdateProjectHandlerTests()
+    {
+        _projectRepository = new Mock<IProjectRepository>();
+        _projectBuilder = new ProjectBuilder();
+
+        _handler = new UpdateProjectHandler(
+            _projectRepository.Object,
+            UserContext.Object,
+            Clock.Object
+        );
+    }
+
+    [Fact]
+    public async Task 正常系_Handle()
+    {
+        // Arrange
+        var project = _projectBuilder.Build();
+        var command = new UpdateProjectCommand(
+            project.Id,
+            "プロジェクト名 - 編集",
+            "プロジェクト説明 - 編集",
+            Guid.NewGuid()
+        );
+
+        _projectRepository
+            .Setup(x => x.GetByIdAsync(project.Id, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(project);
+        _projectRepository
+            .Setup(x => x.UpdateAsync(It.IsAny<Project>(), It.IsAny<CancellationToken>()))
+            .Returns(Task.CompletedTask);
+
+        // Act
+        var result = await _handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        result.Should().Be(Unit.Value);
+        project.Name.Should().Be(command.Name);
+        project.Description.Should().Be(command.Description);
+        project.OwnerId.Should().Be(command.OwnerId);
+        project.UpdatedBy.Should().Be(UserContext.Object.Id);
+        project.UpdatedAt.Should().Be(Clock.Object.Now);
+        _projectRepository.Verify(x => x.GetByIdAsync(project.Id, It.IsAny<CancellationToken>()));
+        _projectRepository.Verify(x => x.UpdateAsync(project, It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task 異常系_Handle_Projectが存在しない場合()
+    {
+        // Arrange
+        var command = new UpdateProjectCommand(
+            Guid.NewGuid(),
+            "プロジェクト名 - 編集",
+            "プロジェクト説明 - 編集",
+            Guid.NewGuid()
+        );
+        _projectRepository
+            .Setup(x => x.GetByIdAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync((Project?)null);
+
+        // Act
+        var act = async () => await _handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        var ex = await act.Should().ThrowAsync<NotFoundException>();
+        ex.Which.ErrorCode.Should().Be("PROJECT_NOT_FOUND");
+        _projectRepository.Verify(x => x.UpdateAsync(It.IsAny<Project>(), It.IsAny<CancellationToken>()), Times.Never);
+    }
+}
