22bffa89a4bdabd15c9d64a838ba6295d6b262f4
kuairen <kaito_127@icloud.com>
2025-09-09 15:59:22 +0000

    refactor(application): :truck: プロジェクトは作成ではなく開始というユビキタス言語とするよう修正

--- WebApi.Application/Commands/Projects/LaunchProject/LaunchProjectCommand.cs
+++ WebApi.Application/Commands/Projects/LaunchProject/LaunchProjectCommand.cs
@@ -2,13 +2,13 @@ using MediatR;
 
 namespace WebApi.Application.Commands.Projects.CreateProject;
 
-public class CreateProjectCommand : IRequest<Guid>
+public class LaunchProjectCommand : IRequest<Guid>
 {
     public string Name { get; }
     public string? Description { get; }
     public Guid OwnerId { get; }
 
-    public CreateProjectCommand(string name, string? description, Guid ownerId)
+    public LaunchProjectCommand(string name, string? description, Guid ownerId)
     {
         Name = name;
         Description = description;
--- WebApi.Application/Commands/Projects/LaunchProject/LaunchProjectHandler.cs
+++ WebApi.Application/Commands/Projects/LaunchProject/LaunchProjectHandler.cs
@@ -6,17 +6,17 @@ using WebApi.Domain.Aggregates.ProjectAggregate;
 
 namespace WebApi.Application.Commands.Projects.CreateProject;
 
-public class CreateProjectHandler : BaseHandler, IRequestHandler<CreateProjectCommand, Guid>
+public class LaunchProjectHandler : BaseHandler, IRequestHandler<LaunchProjectCommand, Guid>
 {
     private readonly IProjectRepository _projectRepository;
 
-    public CreateProjectHandler(IProjectRepository projectRepository, IUserContext userContext, IDateTimeProvider clock)
+    public LaunchProjectHandler(IProjectRepository projectRepository, IUserContext userContext, IDateTimeProvider clock)
         : base(userContext, clock)
     {
         _projectRepository = projectRepository;
     }
 
-    public async Task<Guid> Handle(CreateProjectCommand request, CancellationToken cancellationToken)
+    public async Task<Guid> Handle(LaunchProjectCommand request, CancellationToken cancellationToken)
     {
         var project = new Project(
             request.Name,
