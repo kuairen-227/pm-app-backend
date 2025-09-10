87b04c3a975e712f56a66ab849fe319213e6e8aa
kuairen <kaito_127@icloud.com>
2025-09-10 14:49:45 +0000

    fix(ticket): :pencil2: 誤記修正

--- WebApi.Application/Common/Securiy/Permissions/TicketPermissions.cs
+++ WebApi.Application/Common/Securiy/Permissions/TicketPermissions.cs
@@ -2,6 +2,6 @@ namespace WebApi.Application.Common.Security.Permissions;
 
 public static class TicketPermissions
 {
-    public const string Launch = "TICKET_LAUNCH";
+    public const string Raise = "TICKET_RAISE";
     public const string Delete = "TICKET_DELETE";
 }
