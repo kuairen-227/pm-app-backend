c0e36b64717f163eeac0a6b48bca152904516068
kuairen <kaito_127@icloud.com>
2025-09-11 14:02:14 +0000

    fix(application): :fire: MediatRのパイプラインを利用することでPermissionServiceの注入が不要になったので削除

--- WebApi.Application/Common/BaseHandler.cs
+++ WebApi.Application/Common/BaseHandler.cs
@@ -1,17 +1,14 @@
 using WebApi.Domain.Abstractions;
-using WebApi.Domain.Common.Security;
 
 namespace WebApi.Application.Common;
 
 public abstract class BaseHandler
 {
-    protected readonly IPermissionService PermissionService;
     protected readonly IUserContext UserContext;
     protected readonly IDateTimeProvider Clock;
 
-    protected BaseHandler(IPermissionService permissionService, IUserContext userContext, IDateTimeProvider clock)
+    protected BaseHandler(IUserContext userContext, IDateTimeProvider clock)
     {
-        PermissionService = permissionService;
         UserContext = userContext;
         Clock = clock;
     }
