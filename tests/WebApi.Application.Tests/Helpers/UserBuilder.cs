43298cab04c07a4af74739e9ffaf3778e9353cab
kuairen <kaito_127@icloud.com>
2025-09-12 00:44:15 +0000

    feat(tests): :technologist: アプリケーション層のテスト用ビルダーを作成

---
+++ tests/WebApi.Application.Tests/Helpers/UserBuilder.cs
@@ -0,0 +1,40 @@
+using WebApi.Application.Helpers.Common;
+using WebApi.Domain.Aggregates.UserAggregate;
+
+namespace WebApi.Domain.Tests.Helpers;
+
+public class UserBuilder : BaseBuilder<UserBuilder, User>
+{
+    private string _name = "デフォルトユーザー";
+    private string _email = "default@example.com";
+    private Role.RoleType _role = Role.RoleType.Member;
+
+    public UserBuilder WithName(string name)
+    {
+        _name = name;
+        return this;
+    }
+
+    public UserBuilder WithEmail(string email)
+    {
+        _email = email;
+        return this;
+    }
+
+    public UserBuilder WithRole(Role.RoleType role)
+    {
+        _role = role;
+        return this;
+    }
+
+    public override User Build()
+    {
+        return new User(
+            _name,
+            Email.Create(_email),
+            Role.Create(_role),
+            _createdBy,
+            _clock
+        );
+    }
+}
