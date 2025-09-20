9c1161ebabb5b4c6372130050094fbc488f9e0f4
kuairen <kaito_127@icloud.com>
2025-09-20 07:41:47 +0000

    refactor(application): :fire: 不要なusing削除

--- WebApi.Application/Queries/Projects/ListProjects/ListProjectsQuery.cs
+++ WebApi.Application/Queries/Projects/ListProjects/ListProjectsQuery.cs
@@ -1,7 +1,5 @@
 using MediatR;
-using WebApi.Application.Common.Security;
 using WebApi.Application.Queries.Projects.Dtos;
-using WebApi.Domain.Common.Security.Permissions;
 
 namespace WebApi.Application.Queries.Projects.ListProjects;
 
