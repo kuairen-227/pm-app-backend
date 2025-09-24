7c5fbe9f834c48d6c7c56704ae9bb8921fb929a1
kuairen <kaito_127@icloud.com>
2025-09-24 15:07:40 +0000

    feat(domain): :sparkles: `INotirficationRepository` の作成

---
+++ WebApi.Domain/Abstractions/Repositories/INotificationRepository.cs
@@ -0,0 +1,11 @@
+using WebApi.Domain.Aggregates.NotificationAggregate;
+
+namespace WebApi.Domain.Abstractions.Repositories;
+
+public interface INotificationRepository
+{
+    Task<IEnumerable<Notification>> GetByRecipientAsync(Guid recipientId, CancellationToken cancellationToken = default);
+    Task<Notification?> GetByIdAsync(Guid notificationId, CancellationToken cancellationToken = default);
+    Task AddAsync(Notification notification, CancellationToken cancellationToken = default);
+    Task DeleteAsync(Notification notification, CancellationToken cancellationToken = default);
+}
