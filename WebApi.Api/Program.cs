2f45de4d0c1c7dce6a8a6e6714de10e365b71b84
kuairen <kaito_127@icloud.com>
2025-09-26 16:26:37 +0000

    refactor(api): :bulb: コメント等を追加

--- WebApi.Api/Program.cs
+++ WebApi.Api/Program.cs
@@ -1,16 +1,12 @@
 using WebApi.Application;
+using WebApi.Domain;
 using WebApi.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// ASP.NET Core API 用の DI
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
-builder.Services.AddApplication();
-builder.Services.AddDomain();
-builder.Services.AddInfrastructure(
-    builder.Configuration.GetConnectionString("DefaultConnection")
-        ?? throw new ArgumentNullException("DefaultConnection は必須です")
-);
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontEnd",
@@ -19,13 +15,22 @@ builder.Services.AddCors(options =>
                         .AllowAnyMethod());
 });
 
+// DDD プロジェクトごとの DI
+builder.Services.AddApplication();
+builder.Services.AddDomain();
+builder.Services.AddInfrastructure(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+        ?? throw new ArgumentNullException("DefaultConnection は必須です")
+);
+
+
 var app = builder.Build();
 
+// HTTP リクエストパイプラインの構成
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
-
 app.UseHttpsRedirection();
 app.UseCors("AllowFrontEnd");
 app.UseAuthentication();
