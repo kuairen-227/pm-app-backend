22727626566cd3037ea8345013c01aa418b71d14
kuairen <kaito_127@icloud.com>
2025-10-25 11:48:16 +0000

    fix(ticket): :bug: DTOの型指定ミスを修正

--- WebApi.Application/Queries/Tickets/Dtos/TicketBaseDto.cs
+++ WebApi.Application/Queries/Tickets/Dtos/TicketBaseDto.cs
@@ -5,7 +5,7 @@ public abstract class TicketBaseDto
     public Guid Id { get; init; }
     public string Title { get; init; } = string.Empty;
     public Guid? AssigneeId { get; init; }
-    public DateTime? Deadline { get; init; }
+    public DateOnly? Deadline { get; init; }
     public string Status { get; init; } = string.Empty;
     public string? CompletionCriteria { get; init; }
 }
