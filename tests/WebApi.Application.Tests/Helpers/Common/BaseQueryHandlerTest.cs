ee5cee575909b2a7cedc92c326590e32490dd656
kuairen <kaito_127@icloud.com>
2025-10-25 11:51:33 +0000

    test(tests): :sparkles: クエリハンドラー用テストの基底クラスに `Clock` を追加

--- tests/WebApi.Application.Tests/Helpers/Common/BaseQueryHandlerTest.cs
+++ tests/WebApi.Application.Tests/Helpers/Common/BaseQueryHandlerTest.cs
@@ -1,17 +1,20 @@
 using AutoMapper;
 using Moq;
 using WebApi.Application.Abstractions;
+using WebApi.Tests.Helpers.Fixtures;
 
 namespace WebApi.Application.Tests.Helpers.Common;
 
 public abstract class BaseQueryHandlerTest
 {
-    protected readonly Mock<IUserContext> UserContext;
     protected readonly Mock<IMapper> Mapper;
+    protected readonly Mock<IUserContext> UserContext;
+    protected readonly FakeDateTimeProvider Clock;
 
     protected BaseQueryHandlerTest()
     {
         UserContext = TestHelpers.CreateUserContext();
         Mapper = new Mock<IMapper>();
+        Clock = new FakeDateTimeProvider();
     }
 }
