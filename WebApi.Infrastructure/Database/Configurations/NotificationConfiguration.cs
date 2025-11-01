18d0427a0ae253e6ecd7294f83c2384e8f0d519c
kuairen <kaito_127@icloud.com>
2025-11-01 12:58:27 +0000

    refactor(infrastructure): :bricks: カラム名をドメインモデルより物理的命名に変更

--- WebApi.Infrastructure/Database/Configurations/NotificationConfiguration.cs
+++ WebApi.Infrastructure/Database/Configurations/NotificationConfiguration.cs
@@ -8,6 +8,9 @@ public class NotificationConfiguration : IEntityTypeConfiguration<Notification>
 {
     public void Configure(EntityTypeBuilder<Notification> builder)
     {
+        builder.Property(n => n.RecipientId)
+            .HasColumnName("user_id");
+
         builder.OwnsOne(n => n.Category, category =>
         {
             category.Property(c => c.Value)
