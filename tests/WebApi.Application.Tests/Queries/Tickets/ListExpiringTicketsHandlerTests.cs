534b00f9ebab2605f9de7afc8d31ac3382bb524b
kuairen <kaito_127@icloud.com>
2025-11-09 14:06:39 +0000

    fix(tests): :bug: `AuditInfo` の不足修正

--- tests/WebApi.Application.Tests/Queries/Tickets/ListExpiringTicketsHandlerTests.cs
+++ tests/WebApi.Application.Tests/Queries/Tickets/ListExpiringTicketsHandlerTests.cs
@@ -30,6 +30,10 @@ public class ListExpiringTicketsHandlerTests : BaseQueryHandlerTest
                     Deadline = t.Deadline?.Value,
                     Status = t.Status.Value.ToString(),
                     CompletionCriteria = t.CompletionCriteria,
+                    CreatedBy = t.AuditInfo.CreatedBy,
+                    CreatedAt = t.AuditInfo.CreatedAt,
+                    UpdatedBy = t.AuditInfo.UpdatedBy,
+                    UpdatedAt = t.AuditInfo.UpdatedAt
                 }).ToList());
 
         _handler = new ListExpiringTicketsHandler(
