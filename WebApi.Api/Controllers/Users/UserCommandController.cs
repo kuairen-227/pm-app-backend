ace18e9ce43b89b05cbf7e60bf99f25c92cac4d7
kuairen <kaito_127@icloud.com>
2025-11-23 12:04:42 +0000

    feat(user): :sparkles: `User` Controllerの追加

---
+++ WebApi.Api/Controllers/Users/UserCommandController.cs
@@ -0,0 +1,54 @@
+using Asp.Versioning;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Application.Commands.Users.DeleteUser;
+using WebApi.Application.Commands.Users.RegisterUser;
+
+namespace WebApi.Api.Controllers.Users;
+
+/// <summary>
+/// User（Command）
+/// </summary>
+[ApiController]
+[ApiVersion(1.0)]
+[Route("api/v{version:apiVersion}/users")]
+public class UserCommandController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    public UserCommandController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    /// <summary>
+    /// ユーザー登録
+    /// </summary>
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    public async Task<IActionResult> RegisterAsync([FromBody] RegisterUserCommand command, CancellationToken cancellationToken)
+    {
+        await _mediator.Send(command, cancellationToken);
+        return Created();
+    }
+
+    // TODO: ロール変更はユーザー編集APIに変更したい
+
+    /// <summary>
+    /// ユーザー削除
+    /// </summary>
+    [HttpDelete("{id:guid}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> DeleteAsync(Guid id, CancellationToken cancellationToken)
+    {
+        await _mediator.Send(new DeleteUserCommand(id), cancellationToken);
+        return NoContent();
+    }
+}
---
+++ WebApi.Api/Controllers/Users/UserQueryController.cs
@@ -0,0 +1,38 @@
+using Asp.Versioning;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Application.Queries.Users.Dtos;
+using WebApi.Application.Queries.Users.ListUsers;
+
+namespace WebApi.Api.Controllers.Users;
+
+/// <summary>
+/// User（Query）
+/// </summary>
+[ApiController]
+[ApiVersion(1.0)]
+[Route("api/v{version:apiVersion}/users")]
+public class UserQueryController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    public UserQueryController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    /// <summary>
+    /// ユーザー一覧取得
+    /// </summary>
+    [HttpGet]
+    [ProducesResponseType(typeof(IReadOnlyList<UserDto>), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    public async Task<ActionResult<IReadOnlyList<UserDto>>> ListAllAsync(CancellationToken cancellationToken)
+    {
+        var result = await _mediator.Send(new ListUsersQuery(), cancellationToken);
+        return Ok(result);
+    }
+}
