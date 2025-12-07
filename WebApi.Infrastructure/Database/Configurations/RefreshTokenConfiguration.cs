6f0b48b4bf9235531dfd17bc631c3bd86df79ad5
kuairen <kaito_127@icloud.com>
2025-12-07 08:14:49 +0000

    feat(infrastructure): :card_file_box: `refresh_tokens` と `users` テーブルのリレーション追加

--- WebApi.Infrastructure/Database/Configurations/RefreshTokenConfiguration.cs
+++ WebApi.Infrastructure/Database/Configurations/RefreshTokenConfiguration.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WebApi.Domain.Aggregates.UserAggregate;
 using WebApi.Infrastructure.Database.Configurations.Extensions;
 using WebApi.Infrastructure.Database.Entities;
 
@@ -13,5 +14,10 @@ public class RefreshTokenConfiguration : IEntityTypeConfiguration<RefreshToken>
         {
             a.OwnsAuditInfo();
         });
+
+        builder.HasOne<User>()
+            .WithMany()
+            .HasForeignKey(rt => rt.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
     }
 }
