6b48a637f761e45e2cf09a9bcf6d805ce81f9781
kuairen <kaito_127@icloud.com>
2026-01-14 14:33:44 +0000

    fix(tests): :bug: IntegrationTest 用の `JwtSettings` を注入

--- tests/WebApi.IntegrationTests/TestWebApplicationFactory.cs
+++ tests/WebApi.IntegrationTests/TestWebApplicationFactory.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using WebApi.Infrastructure.Database;
+using WebApi.Infrastructure.Services.AuthService;
 using WebApi.IntegrationTests.Helpers;
 
 namespace WebApi.IntegrationTests;
@@ -33,6 +34,15 @@ public class TestWebApplicationFactory : WebApplicationFactory<Program>
             })
             .AddScheme<AuthenticationSchemeOptions, AuthHandler>(
                 AuthHandler.TestScheme, _ => { });
+
+            // JWT Settings
+            services.Configure<JwtSettings>(options =>
+            {
+                options.SecretKey = "INTEGRATION_TEST_SECRET_KEY_1234567890";
+                options.Issuer = "TestIssuer";
+                options.Audience = "TestAudience";
+                options.AccessTokenExpirationMinutes = 99999999;
+            });
         });
     }
 }
